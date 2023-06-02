28e8bd6612ad2565f967decaeaa70b5dad39b04a
Joe Rivera <dvs_joe@hotmail.com>
2023-06-01 23:18:04 -0400

    Created a function that returns the license section of README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,15 @@ function renderLicenseLink(license) {const licenseLinks = {
 };
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {  if (license) {
+  return `
+## License
+
+This project is licensed under the ${license} license. ${renderLicenseLink(license)}`;
+}
+
+return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {

8f1c6a83da844f41ee72a6ca09f3ea470a936562
Joe Rivera <dvs_joe@hotmail.com>
2023-06-01 23:13:50 -0400

    Created a function that returns the license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,8 +15,11 @@ return '';
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {const licenseLinks = {
+  'MIT': 'https://opensource.org/licenses/MIT',
+  'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+  }
+};
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}

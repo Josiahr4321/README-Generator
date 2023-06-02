f91f8754549d28aae88cb9b922e772901249121a
Joe Rivera <dvs_joe@hotmail.com>
2023-06-01 23:01:29 -0400

    created a function write readme

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ const questions = [{
   },];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`README file "${fileName}" has been created.`);
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}

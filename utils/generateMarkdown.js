9eda8a1f031ca82526997a71e1873a988d317904
Joe Rivera <dvs_joe@hotmail.com>
2023-06-01 23:11:01 -0400

    Created the function that returns a license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {const licenseBadges = {
+  'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+  'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+  
+};
+
+if (license && licenseBadges.hasOwnProperty(license)) {
+  return `![License](${licenseBadges[license]})`;
+}
+
+return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string

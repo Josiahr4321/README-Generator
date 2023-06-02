ce30d36798a79e1af97932dc389392e2bed33a73
Joe Rivera <dvs_joe@hotmail.com>
2023-06-01 22:58:06 -0400

    Added questions for the readme generator

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Provide a brief description of your project:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'What are the installation instructions for your project?',
+    name: 'installation',
+  },];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
